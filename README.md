9f6f4edd24b9cba67229b124c1ef7ada42330e4a
Gennady Kovshenin <gennady@kovshenin.com>
2015-03-20 21:18:01 +0500

    Initial

---
+++ README.md
@@ -0,0 +1,6 @@
+Garmin Connect Running Stats for WordPress
+------------------------------------------
+
+Show off your excellent running stats on your WordPress blog driven by your Garmin Connect public profile.
+
+**important**: this plugin should not be used in production, it's written as a demo case for a transparent HTTP caching layer for WordPress that is used by Pressjitsu. It's purpose is to make requests without even bothering to cache them, on each and every pageload.
---
+++ wp-gc-running-stats.php
@@ -0,0 +1,58 @@
+<?php
+	/*
+	 * Plugin Name: Garmin Connect Running Stats for WordPress
+	 * Author: Gennady Kovshenin
+	 * Description: Show off your excellent running stats on your WordPress blog driven by your Garmin Connect public profile.
+	 * Version: 0.1
+	 */
+
+	class GC_Running_Stats_Widget extends WP_Widget {
+
+		public function __construct() {
+			parent::__construct(
+				'gc-running-stats',
+				'Garmin Connect Running Stats',
+				array( 'description' => 'Show off your excellent running stats on your WordPress blog driven by your Garmin Connect public profile.' )
+			);
+		}
+
+		public function form( $instance ) {
+			$username = ! empty( $instance['username'] ) ? $instance['username'] : 'soulseekah';
+			?>
+				<p>
+					<label for="username">Username: </label> 
+					<input class="widefat" id="username" name="username" type="text" value="<?php echo esc_attr( $username ); ?>">
+				</p>
+			<?php 
+		}
+
+		public function widget( $args, $instance ) {
+
+			echo $args['before_widget'];
+
+			echo $args['before_title'] . apply_filters( 'widget_title', 'My Garmin Connect Stats' ). $args['after_title'];
+
+			$username = ! empty( $instance['username'] ) ? $instance['username'] : 'soulseekah';
+			/** I'm never bothered with caching, because Pressjitsu's transparent HTTP cache layer will take care of me :) */
+			$start = microtime( true );
+			$response = wp_remote_get( 'https://connect.garmin.com/proxy/userstats-service/statistics/' . $username );
+			$data = json_decode( wp_remote_retrieve_body( $response ) );
+			
+			?>
+				<!-- Loaded in <?php echo esc_html( microtime( true ) - $start ); ?> ms -->
+				<ul>
+					<li>Total activities: <?php echo esc_html( $data->userMetrics[0]->totalActivities ); ?></li>
+					<li>Total distance: <?php echo esc_html( round( $data->userMetrics[0]->totalDistance / 1000 ) ); ?> km</li>
+					<li>Total calories: <?php echo esc_html( $data->userMetrics[0]->totalCalories ); ?> C</li>
+				</ul>
+
+				<p><a href="http://connect.garmin.com/profile/<?php echo esc_attr( $username ); ?>">Friend me</a> and let's be running together :)</p>
+			<?php
+
+			echo $args['after_widget'];
+		}
+	}
+
+	add_action( 'widgets_init', function() {
+		register_widget( 'GC_Running_Stats_Widget' );
+	} );
